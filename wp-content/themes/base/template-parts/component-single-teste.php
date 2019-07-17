a6a7573a3e14bcfdab4964aa189fd34e06bf7284
unknown <l.oliveira21p@gmail.com>
2019-07-17 12:03:09 -0300

    Ajeitar em casa kk

--- wp-content/themes/base/template-parts/component-single-teste.php
+++ wp-content/themes/base/template-parts/component-single-teste.php
@@ -1,5 +1,11 @@
+<?php
+if(is_singular() && !is_admin()):
+ ?>
 <div class="container">
   <h2><?php the_title();?></h2>
   <?php the_content();?>
   <?php  wp_reset_query();?>
 </div>
+<?php
+endif;
+ ?>
